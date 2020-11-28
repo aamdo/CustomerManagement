9300f2f09041299a5116ca30d9a6e575c1e25c57
a0555170767 <a0555170767@gmail.com>
2020-11-28 22:15:59 +0300

    p8-7:12

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    context = {'orders' :orders,'customers' :customers }
+    
+    return render(request,'accounts/dashboard.html', context)
+
+def homee(request):
+    ps = Product.objects.all()
+    context = {'ps': ps}
+    return render(request,'accounts/homee.html',context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    ps = Product.objects.all()
+    context = {'ps': ps}
+    return render(request,'accounts/products.html',context)
 
 
 def customer(request):
