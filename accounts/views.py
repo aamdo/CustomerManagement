aae7cfcc9cb7daf8b828ae3954f4f01dac653297
a0555170767 <a0555170767@gmail.com>
2020-11-30 22:07:41 +0300

    pt11-5:10

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
@@ -52,13 +53,17 @@ def customer(request,pk_test):
     return render(request,'accounts/customer.html', context)
 
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer,Order)
+    
+    customer = Customer.objects.get(id=pk)
+    form = OrderForm(initial={'customer':customer})
+    
     if request.method=="POST":
         form=OrderForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('/',pk)
     context = {'form':form}
     return render(request,'accounts/order_form.html',context)
 
