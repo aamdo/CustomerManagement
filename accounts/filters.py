5af1593fb7f2d03f20e76dd184454607c41efefb
aamdo <a0555170767@gmail.com>
2020-12-07 14:16:54 +0300

    pt12-9:12

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django_filters import DateFilter
 from django_filters import parse_version
 from .models import *
 
@@ -6,4 +7,6 @@ class OrderFilter(django_filters.FilterSet):
     class Meta:
         model=Order
         fields = '__all__'
+        exclude = ['customer','date_created']
+        
     
