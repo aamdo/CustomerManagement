b896962038d538fb1a774c1c94b4ab34b560bc3c
a0555170767 <a0555170767@gmail.com>
2020-12-08 22:27:14 +0300

    pt12:end

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,9 +1,14 @@
 import django_filters
-from django_filters import DateFilter
+from django_filters import DateFilter,CharFilter
 from django_filters import parse_version
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
+    start_date = DateFilter(field_name='date_created',lookup_expr='gte',label='التاريخ يبدأ ب')
+    end_date = DateFilter(field_name='date_created',lookup_expr='lte',label='التاريخ ينتهي ب')
+    note = CharFilter(field_name='note',lookup_expr='icontains')
+
+    
     class Meta:
         model=Order
         fields = '__all__'
