8bb388d7f2392c0a9a2f8e4eb4a7b8b757dd3d52
aamdo <a0555170767@gmail.com>
2020-12-13 14:10:44 +0300

    pt14-34:00

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,4 +17,4 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
         fields = ['username','email','password1','password2']
-                
+        
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 from .models import *
-from .forms import OrderForm,CreateUserForm
+from .forms import  OrderForm,CreateUserForm
 from .filters import OrderFilter
 from django.core.paginator import Paginator
+
+
 
 # Create your views here.
 
@@ -15,18 +21,34 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for '+user)
+            return redirect('login')
 
-        
-    
-    
     context = {'form':form}
     return render(request,'accounts/register.html',context)
 
+
 def loginPage(request):
-    context = { }
+    if request.method == 'POST':
+       username = request.POST.get('username')
+       password =  request.POST.get('password')
+       user = authenticate(request,username=username,password=password)
+       if user is not None:
+            login(request,user)
+            return redirect('home')
+       else:
+            messages.info(request,'username or password is incorrect')
+            
+    context = {}
+            
     return render(request,'accounts/login.html',context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
