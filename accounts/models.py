1474cf69fdf156ff9ced0704dd90e4065e2824be
aamdo <a0555170767@gmail.com>
2020-12-15 14:27:54 +0300

    pt17-18:00

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,9 @@ class Customer(models.Model):
     name = models.CharField(max_length=20,null=True)
     phone = models.CharField(max_length=20,null=True)
     email = models.EmailField(max_length=50,null=True)
+    profile_pic = models.ImageField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name
